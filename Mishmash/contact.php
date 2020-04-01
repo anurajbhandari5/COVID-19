0ee179f8ce2ce3d914f7994bb41d85856bf17b95
anurajbhandari5 <56197368+anurajbhandari5@users.noreply.github.com>
2020-04-01 21:25:26 +0530

    Add files via upload

---
+++ Mishmash/contact.php
@@ -0,0 +1,314 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="O.E.C.D">
+	<meta name="author" content="mismash">
+	<title>4witchers</title>
+	<link rel="favicon" href="assets/images/favicon.png">
+	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+	<!-- Custom styles for our template -->
+	<link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="assets/js/html5shiv.js"></script>
+	<script src="assets/js/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+	<!-- Fixed navbar -->
+	<div class="navbar navbar-inverse">
+		<div class="container">
+			<div class="navbar-header">
+				<!-- Button for smallest screens -->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+				<a class="navbar-brand" href="index.html">
+					<img src="assets/images/logo.png" alt="Techro HTML5 template"></a>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav pull-right mainNav">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="about.html">About</a></li>
+						<li><a href="courses.html">Courses</a></li>
+					<li><a href="price.html">Price</a></li>
+					<li><a href="videos.html">Videos</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages <b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="sidebar-right.html">Right Sidebar</a></li>
+							<li><a href="#">Dummy Link1</a></li>
+							<li><a href="#">Dummy Link2</a></li>
+							<li><a href="#">Dummy Link3</a></li>
+						</ul>
+					</li>
+					<li class="active"><a href="contact.html">Contact</a></li>
+				</ul>
+			</div>
+			<!--/.nav-collapse -->
+		</div>
+	</div>
+	<!-- /.navbar -->
+
+		<header id="head" class="secondary">
+            <div class="container">
+                    <h1>Contact Us</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing eliras scele!</p>
+                </div>
+    </header>
+
+
+	<!-- container -->
+	<div class="container">
+				<div class="row">
+					<div class="col-md-8">
+						<h3 class="section-title">Your Message</h3>
+						<p>
+						Lorem Ipsum is inting and typesetting in simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the is dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+						</p>
+						
+					  
+		<!--NOTE: Update your email Id in "contact_me.php" file in order to receive emails from your contact form-->
+		<form name="sentMessage" id="contactForm" method="post" novalidate> 
+		<div class="control-group">
+		<div class="controls">
+		<input type="text" class="form-control" 
+		placeholder="Full Name" name="name" id="name" required
+		data-validation-required-message="Please enter your name" />
+		<p class="help-block"></p>
+		</div>
+		</div> 	
+		<div class="control-group">
+		<div class="controls">
+		<input type="email" class="form-control" placeholder="Email" 
+		id="email" name="email" required
+		data-validation-required-message="Please enter your email" />
+		</div>
+		</div> 	
+
+		<div class="control-group">
+		<div class="controls">
+		<textarea rows="10" cols="100" class="form-control" 
+		placeholder="Message" id="message" name="message" required
+		data-validation-required-message="Please enter your message" minlength="5" 
+		data-validation-minlength-message="Min 5 characters" 
+		maxlength="999" style="resize:none"></textarea>
+		</div>
+		</div> 		 
+		<div id="success"> </div> <!-- For success/fail messages -->
+		<button type="submit" class="btn btn-primary pull-right" name="submit">Send</button><br />
+		</form>
+
+
+<?php 
+
+$conn=mysqli_connect("localhost","root","","course"); //host,usernme,passwrd,databasename
+
+if(isset($_POST['submit'])){
+	
+	$name=mysqli_real_escape_string($conn,$_POST['name']);
+	$email=mysqli_real_escape_string($conn,$_POST['email']);
+	$message=mysqli_real_escape_string($conn,$_POST['message']);
+  if(empty($name) OR empty($email) OR empty($message)){
+    
+   header("Location: contact.php?message=empty+field"); }
+   else{
+
+
+$sql="INSERT INTO `onlinee`(`id`, `name`, `email`, `message`) VALUES ('$name','$email','$message');";
+//to run the query//to run the query
+if(mysqli_query($conn, $sql)){
+      header("Location:  contact.php?message=successfully+registered");
+}else{
+  header("Location:  contact.php?message=registration+failed");
+
+}
+
+}
+}
+?>
+
+
+
+
+
+
+
+
+					</div>
+					<div class="col-md-4">
+						<div class="row">
+							<div class="col-md-6">
+								<h3 class="section-title">Office Address</h3>
+								<div class="contact-info">
+									<h5>Address</h5>
+									<p>5th Street,Rajori Garden,New Delhi</p>
+									
+									<h5>Email</h5>
+									<p>anurajbhandari5@gmail.com</p>
+									
+									<h5>Phone</h5>
+									<p>+09 123 1234 123</p>
+								</div>
+							</div> 
+						</div> 						
+					</div>
+				</div>
+			</div>
+	<!-- /container -->
+
+	 <footer id="footer">
+ 
+		<div class="container">
+   <div class="row">
+  <div class="footerbottom">
+    <div class="col-md-3 col-sm-6">
+      <div class="footerwidget">
+        <h4>
+          Course Categories
+        </h4>
+        <div class="menu-course">
+          <ul class="menu">
+            <li><a href="#">
+                List of Technology 
+              </a>
+            </li>
+            <li><a href="#">
+                List of Business
+              </a>
+            </li>
+            <li><a href="#">
+                List of Photography
+              </a>
+            </li>
+            <li><a href="#">
+               List of Language
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-6">
+      <div class="footerwidget">
+        <h4>
+          Products Categories
+        </h4>
+        <div class="menu-course">
+          <ul class="menu">
+            <li> <a href="#">
+                Individual Plans  </a>
+            </li>
+            <li><a href="#">
+                Business Plans
+              </a>
+            </li>
+            <li><a href="#">
+                Free Trial
+              </a>
+            </li>
+            <li><a href="#">
+                Academic
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-6">
+      <div class="footerwidget">
+        <h4>
+          Browse by Categories
+        </h4>
+        <div class="menu-course">
+          <ul class="menu">
+            <li><a href="#">
+                All Courses
+              </a>
+            </li>
+            <li> <a href="#">
+                All Instructors
+              </a>
+            </li>
+            <li><a href="#">
+                All Members
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                All Groups
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-6"> 
+            	<div class="footerwidget"> 
+                         <h4>Contact</h4> 
+                        <p>Lorem reksi this dummy text unde omnis iste natus error sit volupum</p>
+            <div class="contact-info"> 
+            <i class="fa fa-map-marker"></i>Rajori Garden , Newd delhi<br>
+            <i class="fa fa-phone"></i>+00 123 156 711 <br>
+             <i class="fa fa-envelope-o"></i> youremail@email.com
+              </div> 
+                </div><!-- end widget --> 
+    </div>
+  </div>
+</div>
+			<div class="social text-center">
+				<a href="#"><i class="fa fa-twitter"></i></a>
+				<a href="#"><i class="fa fa-facebook"></i></a>
+				<a href="#"><i class="fa fa-dribbble"></i></a>
+				<a href="#"><i class="fa fa-flickr"></i></a>
+				<a href="#"><i class="fa fa-github"></i></a>
+			</div>
+
+			<div class="clear"></div>
+			<!--CLEAR FLOATS-->
+		</div>
+		<div class="footer2">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-6 panel">
+						<div class="panel-body">
+							<p class="simplenav">
+								<a href="index.html">Home</a> | 
+								<a href="about.html">About</a> |
+								<a href="courses.html">Courses</a> |
+								<a href="price.html">Price</a> |
+								<a href="videos.html">Videos</a> |
+								<a href="contact.html">Contact</a>
+							</p>
+						</div>
+					</div>
+
+					
+
+				</div>
+				<!-- /row of panels -->
+			</div>
+		</div>
+	</footer>
+
+
+	<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+ <script src="contact/jqBootstrapValidation.js"></script>
+ <script src="contact/contact_me.js"></script>
+	<script src="assets/js/custom.js"></script>
+
+	<!-- Google Maps -->
+	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+	<script src="assets/js/google-map.js"></script>
+
+
+</body>
+</html>
